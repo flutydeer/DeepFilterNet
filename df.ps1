a79f2a248cc755728bb606aed2dbbbf954f209ff
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2023-01-08 16:48:06 +0800

    Support for batch processing

--- df.ps1
+++ df.ps1
@@ -1,6 +1,6 @@
 $env:PYTHONPATH=".\DeepFilterNet"
-Set-Location .\venv\Scripts
-./Activate
-Set-Location ..
-Set-Location ..
-python DeepFilterNet/df/enhance.py -m DeepFilterNet2 $args[0] --model-base-dir ".\models\DeepFilterNet2" --output-dir ".\output"
+& ./venv/Scripts/Activate.ps1
+foreach($item in $args)
+{
+    python DeepFilterNet/df/enhance.py -m DeepFilterNet2 $item --model-base-dir ".\models\DeepFilterNet2" --output-dir ".\output"
+}
